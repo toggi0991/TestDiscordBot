6410a52f4cdc96e8bfc032e224080b3b29bdd0ca
toggi0991 <60846780+toggi0991@users.noreply.github.com>
2021-02-15 21:09:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(client.user.id)
-    print("start to" + client.user.name)
     await client.change_presence(activity=discord.Game(name='Destiny 2', type=1))
 
 @client.event
