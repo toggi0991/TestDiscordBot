9483b117156dfededb20b086d1c846165414fe4f
toggi0991 <60846780+toggi0991@users.noreply.github.com>
2021-02-15 20:25:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ async def on_ready():
     print("start to" + client.user.name)
     await client.change_presence(activity=discord.Game(name='Destiny 2', type=1))
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('!안녕'):
+        await client.send_message(message.channel, "안녕하세요")
 
-ClientCode == os.environ["BOT_TOKEN"]
+
+
+ClientCode = os.environ["BOT_TOKEN"]
 client.run(ClientCode)
