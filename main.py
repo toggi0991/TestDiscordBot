d1a9249b1cb65860900b905ad6e5f1555dc35d54
toggi0991 <60846780+toggi0991@users.noreply.github.com>
2021-02-15 20:40:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ client = discord.Client()
 async def on_ready():
     print(client.user.id)
     print("start to" + client.user.name)
-    await client.change_presence(activity=discord.Game(name='Destiny 2', type=1))
+    await client.change_presence(activity=discord.Game(name='Destiny', type=1))
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!안녕'):
-        await client.send_message(message.channel, "안녕하세요")
+    if message.content.startswith('hi'):
+        await client.send_message(message.channel, "hello")
 
 
 
