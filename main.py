72ab7eb63fd0aebc734ea564ff3a3b6909338b74
toggi0991 <60846780+toggi0991@users.noreply.github.com>
2021-02-15 20:18:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ async def on_ready():
     print("start to" + client.user.name)
     await client.change_presence(activity=discord.Game(name='Destiny 2', type=1))
 
+
+ClientCode == os.environ["BOT_TOKEN"]
 client.run(ClientCode)
