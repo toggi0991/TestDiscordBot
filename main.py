f1c7d8ed46856ce14a53e252f7865dd43ee9799d
toggi0991 <60846780+toggi0991@users.noreply.github.com>
2021-02-16 21:46:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ client = discord.Client()
 async def on_ready():
     await client.change_presence(activity=discord.Game(name='Destiny 2', type=1))
 
+async def on_message():
+    await client.channel.send('bot is update')
+
 @client.event
 async def on_message(message):
     if message.content.startswith('hi'):
